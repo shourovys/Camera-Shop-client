7140795c48c9d00c01f5908ebb4b6a61153d98bf
Shourov Saha <shourovys@gmail.com>
2021-11-19 15:08:55 +0600

    product done

---
+++ src/Data/product.js
@@ -0,0 +1,40 @@
+const productData = [{
+  "_id": 1,
+  "name": "Canon, EOS 750D",
+  "price": 392,
+  "image": "https://images.unsplash.com/photo-1513555633610-d912838b8ac5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGNhbWVyYSUyMHByb2R1Y3R8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
+}, {
+  "_id": 2,
+  "name": "SONY, ILCE-7M3",
+  "price": 751,
+  "image": "https://images.unsplash.com/photo-1628270949029-f6fb33e8b7b9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80"
+}, {
+  "_id": 3,
+  "name": "FUJIFILM, X100T",
+  "price": {
+    "$numberInt": "251"
+},
+  "image": "https://images.unsplash.com/photo-1502920917128-1aa500764cbd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80"
+}, {
+  "_id": 4,
+  "name": "NIKON CORPORATION, NIKON D7000",
+  "price": {
+    "$numberInt": "211"
+},
+  "image": "https://images.unsplash.com/photo-1452780212940-6f5c0d14d848?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80"
+}, {
+  "_id": 5,
+  "name": "NIKON CORPORATION, NIKON D750",
+  "price": {
+    "$numberInt": "349"
+},
+  "image": "https://images.unsplash.com/photo-1515081321129-b3c138ab28dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=725&q=80"
+}, {
+  "_id": 6,
+  "name": "Canon, EOS 5DS R",
+  "price": {
+    "$numberInt": "770"
+},
+  "image": "https://images.unsplash.com/photo-1586527484765-979a20639316?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80"
+}]
+export default productData
